b0328a866b4060060676094bf3cf6c4b3e385039
Pirate Dunbar <yarr@piratedunbar.com>
2014-02-02 14:27:12 -0500

    Update default.rb

--- providers/default.rb
+++ providers/default.rb
@@ -4,7 +4,7 @@ action :install do
 	directory new_resource.target_dir do
 		owner new_resource.owner
 		group new_resource.group
-		mode 0755
+		mode 0775
 		recursive true
 	end
 
@@ -12,7 +12,7 @@ action :install do
 		source new_resource.source
 		owner new_resource.owner
 		group new_resource.group
-		mode 0755
+		mode 0775
 
 		action :create_if_missing
 	end
